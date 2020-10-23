18bcf567572d116e6267a4d3836e16b9a0508597
Jan Gevaert <jan.gevaert@gmail.com>
2020-10-23 08:55:49 -0600

    Initial commit

---
+++ src/FirstTwo2.java
@@ -0,0 +1,12 @@
+public class FirstTwo2 {
+  public String firstTwo(String str) {
+    int len = str.length();
+
+    if (len < 2) {
+      return str;
+    } else {
+      return str.substring(0, 2);
+    }
+  }
+
+}
